6f540091622676c31af91694acddd91118675080
AgungFirmansyah09 <firmansya15agung09@gmail.com>
2026-05-26 10:07:43 +0700

    print-QR

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+// session_start();
 include 'function.php';
 
 if (isset($_POST["login"])) {
--- master_qrcode.php
+++ master_qrcode.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+// session_start();
 require 'function.php';
 
 if (!isset($_SESSION['login'])) {
